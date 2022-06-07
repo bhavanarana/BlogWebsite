223fa8739d97ef2dc0cf1fd14f3c1c3a047ba835
Bhavana Rana <4bhavanarana@gmail.com>
2022-06-07 23:16:46 +0530

    use INSERT and SELECT operations

--- php/add.php
+++ php/add.php
@@ -1,16 +1,33 @@
-<?php include "bootstrap.php" ?>
-<?php include "nav.php" ?>
+<html>
+<?php 
+include 'bootstrap.php'; 
+include 'db.php' ;
+?>
 <body>
+  <?php include 'nav.php'; ?>
     <div class="container add-container">
+      <form action = "" method = "POST">
 <div class="mb-3">
   <label for="title" class="form-label">Title</label>
-  <input type="text" class="form-control" class="title">
+  <input type="text" class="form-control" name="title" class="title">
 </div>
 <div class="mb-3">
   <label for="desc" class="form-label">Description</label>
-  <textarea class="form-control" class="desc" rows="3"></textarea>
-  <a class="btn btn-primary button mt-4" href="#" role="button"> + Create Blog</a>
+  <textarea class="form-control" class="desc" name = "desc" rows="3"></textarea>
+  <input type="submit" class="btn btn-primary button mt-4" name="submit" value="+Create blog">
   </div>
+</form>
 </div>
-<?php include "db.php" ?>
+<?php 
+if(isset($_REQUEST['info'])=="added"){?>
+<!--alert box-->
+<div class="container">
+<div class="alert alert-success" role="alert">
+  You have Successfully uploaded your Blog
+</div>
+</div>
+<?php }?>
+
+
 </body>
+</html>
---
+++ php/view.php
@@ -0,0 +1,31 @@
+<html>
+  <?php
+  include 'bootstrap.php';
+  include 'db.php';
+  $select = 'SELECT * FROM entries';
+  $query_select = mysqli_query($conn, $select);
+  if (!$query_select) {
+      echo 'Failed to fetch details';
+  }
+  ?>
+<body>
+    <?php include 'nav.php'; ?>
+    <div class="container">
+        <div class="row">
+            <h1>Blogs</h1>
+        </div>
+        <div class="row">
+            <?php foreach ($query_select as $value) { ?>
+        <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $value['Title']; ?></h5>
+    <p class="card-text"><?php echo $value['Description']; ?></p>
+    <a href="#" class="btn btn-primary">Read</a>
+  </div>
+</div>
+  <?php } ?>
+        </div>
+    </div>
+
+</body>
+</html>
