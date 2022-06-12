8c7ea1b8d4d5377a625d97e6f90e42a08af3fdc8
Bhavana Rana <4bhavanarana@gmail.com>
2022-06-12 19:40:27 +0530

    resolve problem

---
+++ php/signin.php
@@ -0,0 +1,38 @@
+<?php session_start();
+include 'db.php';
+if (isset($_POST['signin'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $query = "SELECT * FROM users WHERE email='$email'";
+    $select_query = mysqli_query($conn, $query);
+    foreach ($select_query as $value) {
+        $match_password = password_verify($password, $value['password']);
+        if ($match_password) {
+            $_SESSION['email'] = $email;
+            header('Location: index.php');
+            exit();
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'bootstrap.php'; ?>
+<body>
+<div class="container">
+    <h1>Sign In</h1>
+    <form method="POST">
+  <div class="mb-3">
+    <label for="email" class="form-label">Email address</label>
+    <input type="email" name="email" class="form-control" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" name="password" class="form-control">
+  </div>
+  <input type="submit" value="Sign In" name="signin" class="btn btn-primary float-end">
+</form>
+    </div>
+</body>
+</html>
--- php/signup.php
+++ php/signup.php
@@ -1,11 +1,24 @@
-<?php include 'db.php';?>
+<?php include 'db.php';   
+ if (isset($_POST['signup'])) {
+     $name = $_POST['name'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+     $secure_password = password_hash($password, PASSWORD_DEFAULT);
+     $query = "INSERT INTO users (name, email, password) VALUES ('$name','$email','$secure_password')";
+     $query_run = mysqli_query($conn, $query);
+     if ($query_run) {
+         header('Location: signin.php');
+     }
+ }
+ ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php include "bootstrap.php";?>
+<?php include 'bootstrap.php'; ?>
 
 <body>
     <div class="container">
     <form method="POST">
+        <h1>Sign Up</h1>
     <div class="mb-3">
     <label for="name" class="form-label">Name</label>
     <input type="text" name="name" class="form-control">
@@ -19,24 +32,9 @@
     <label for="password" class="form-label">Password</label>
     <input type="password" name="password" class="form-control">
   </div>
-  <button type="submit" name="signup" class="btn btn-primary">Sign Up</button>
+  <input type="submit" value="Sign Up" name="signup" class="btn btn-primary float-start">
 </form>
     </div>
-
-    <?php 
-    if(isset($_POST['signup'])){
-        $name=$_POST['name'];
-        $email=$_POST['email'];
-        $password=$_POST['password'];
-        $secure_password=password_hash($password, PASSWORD_DEFAULT);
-        $query="INSERT INTO logindetails (name, email, password) VALUES ('$name','$email','$secure_password')";
-        $query_run=mysqli_query($conn,$query);
-        if($query_run){
-      header("Location: signin.php");
-    }
-    
-    
-    ?>
     
 </body>
 </html>
