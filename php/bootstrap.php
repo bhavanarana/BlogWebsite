214a089b1dc95c5e3f928925cad049da0eff7c52
Bhavana <4bhavanarana@gmail.com>
2022-06-27 21:17:12 +0530

    improve ui

--- php/bootstrap.php
+++ php/bootstrap.php
@@ -8,5 +8,6 @@
 <link href="style.css?v=<?php echo time(); ?>" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Stardos+Stencil:wght@400;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- php/signup.php
+++ php/signup.php
@@ -21,21 +21,30 @@ if (isset($_POST['signup'])) {
 </head>
 
 <body>
-  <div class="container">
-    <form method="POST" id="signup-form">
+  <div class="container signup-container">
+    <form method="POST" class="form" id="signup-form">
       <h1>Sign Up</h1>
-      <div class="mb-3">
+      <div class="mb-3 input-form success">
         <label for="name" class="form-label">Name</label>
-        <input type="text" id="name" name="name" class="form-control">
+        <input type="text" id="name" name="name" class="form-control input-form">
+        <i class="fa fa-check-circle fa-1x"></i>
+        <i class="fa fa-exclamation-circle fa-1x"></i>
+        <small>Error Message</small>
       </div>
-      <div class="mb-3">
+      <div class="mb-3 input-form error">
         <label for="email" class="form-label">Email address</label>
         <input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <i class="fa fa-check-circle fa-1x"></i>
+        <i class="fa fa-exclamation-circle fa-1x"></i>
+        <small>Error Message</small>
       </div>
-      <div class="mb-3">
+      <div class="mb-3 input-form">
         <label for="password" class="form-label">Password</label>
         <input type="password" id="password" name="password" class="form-control">
+        <i class="fa fa-check-circle fa-1x"></i>
+        <i class="fa fa-exclamation-circle fa-1x"></i>
+        <small>Error Message</small>
       </div>
       <input type="submit" value="Sign Up" name="signup" class="btn btn-primary float-start">
     </form>
@@ -45,34 +54,6 @@ if (isset($_POST['signup'])) {
     let name = document.getElementById('name');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      alert('submited')
-    })
-    name.addEventListener('input', validation);
-    email.addEventListener('input', validation);
-    password.addEventListener('input', validation);
-
-    function validation(e) {
-      let target = e.target.name;
-      if (target == name) {
-        //name validation
-      }
-      if (target == email) {
-        //email validation
-        const emailValid = /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
-        if (emailValid.test(e.target.value)) { // test is used in which we want to search or query our condition
-          consoole.log(target.classList.add('valid')); //  Adds the specified token(s) to the list.
-          console.log(target.classList.remove('invalid')); // remove the specified token(s) to the list.
-        } else {
-          target.classList.add('invalid');
-          target.classList.add('valid');
-        }
-      }
-      if (target == password) {
-        //password validation
-      }
-    }
   </script>
 </body>
 
