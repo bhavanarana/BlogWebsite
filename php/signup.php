415bea60dc0b52906ba0db047b32b7fdcf3b9672
Bhavana <4bhavanarana@gmail.com>
2022-06-27 22:40:51 +0530

    added validation for empty blocks

--- php/signup.php
+++ php/signup.php
@@ -24,14 +24,14 @@ if (isset($_POST['signup'])) {
   <div class="container signup-container">
     <form method="POST" class="form" id="signup-form">
       <h1>Sign Up</h1>
-      <div class="mb-3 input-form success">
-        <label for="name" class="form-label">Name</label>
+      <div class="mb-3 input-form">
+        <label for="name" class="form-label">Username</label>
         <input type="text" id="name" name="name" class="form-control input-form">
         <i class="fa fa-check-circle fa-1x"></i>
         <i class="fa fa-exclamation-circle fa-1x"></i>
         <small>Error Message</small>
       </div>
-      <div class="mb-3 input-form error">
+      <div class="mb-3 input-form">
         <label for="email" class="form-label">Email address</label>
         <input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
@@ -54,6 +54,45 @@ if (isset($_POST['signup'])) {
     let name = document.getElementById('name');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      checkValidation();
+    });
+
+    function checkValidation() {
+      let nameValue = name.value.trim();
+      let emailValue = email.value.trim();
+      let passwordValue = password.value.trim();
+      let emailValidate = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+      if (nameValue == "" || nameValue == null) {
+        error(name, 'Username Cannot be blank');
+      } else {
+        success(name);
+      }
+      if (emailValue == "" || emailValue == null) {
+        error(email, 'Email Cannot be blank');
+      } else {
+        success(email);
+      }
+      if (passwordValue == "" || passwordValue == null) {
+        error(password, 'password Cannot be blank');
+      } else {
+        success(password);
+      }
+
+    }
+
+    function success(input) {
+      inputForm = input.parentElement; //input-form access
+      inputForm.className = 'input-form success';
+    }
+
+    function error(input, message) {
+      inputForm = input.parentElement; //input-form access
+      errorMsg = inputForm.querySelector('small');
+      errorMsg.innerText = message;
+      inputForm.className = 'input-form error';
+    }
   </script>
 </body>
 
